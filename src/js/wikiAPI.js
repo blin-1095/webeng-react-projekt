3fc054ca1030aff8c287221432ec70733dedf435
Lukas <lukas.zwaller@gmx.de>
2021-06-19 14:44:45 +0200

    Small fixes

--- src/js/wikiAPI.js
+++ src/js/wikiAPI.js
@@ -22,16 +22,16 @@ export async function reverseGeocoding(lon, lat, setWikiResult, setWikiResultUrl
     if (lon != undefined && lat != undefined){
         const add = osm_json.display_name.split(', ');
         const pos = {streetNo: add[0], street: add[1], suburb: add[2], city: add[3], county: add[4], country: add[5], zip: add[6], state: add[7] };
-        const betterCity = osm_json.address.village || osm_json.address.city_district || osm_json.address.town;
+        const betterCity = osm_json.address.village || osm_json.address.city_district || osm_json.address.town || osm_json.address.city;
         
-        const wiki_url = `https://en.wikipedia.org/w/api.php?origin=*&action=query&list=search&srsearch=${encodeURIComponent(betterCity)}&format=json`;
+        const wiki_url = `https://de.wikipedia.org/w/api.php?origin=*&action=query&list=search&srsearch=${encodeURIComponent(betterCity)}&format=json`;
         console.log(wiki_url);
         const wiki_response = await fetch(wiki_url);
         const wiki_json = await wiki_response.json();
 
         try{
             const pageId = wiki_json.query.search[0].pageid;
-            const urlPageId = `https://en.wikipedia.org/w/api.php?origin=*&action=query&prop=info&pageids=${pageId}&inprop=url&format=json`;
+            const urlPageId = `https://de.wikipedia.org/w/api.php?origin=*&action=query&prop=info&pageids=${pageId}&inprop=url&format=json`;
 
             const wiki_page_response = await fetch(urlPageId);
             const wiki_page = await wiki_page_response.json();
