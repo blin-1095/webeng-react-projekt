38c8079c8be3d75c70a1d1e70bc6f9b60ec18db7
Alessandra Woods <alessandrawoods@gmail.com>
2021-06-19 17:00:47 +0200

    add design elements and logo

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -17,28 +17,32 @@ import {
   Col,
   Button
 } from 'framework7-react';
+import logo1 from '/src/images/logo1.png'
+import mapedia from '/src/images/Mapedia.svg'
 
 import MapObj from '../js/map';
 
 const HomePage = () => (
   <Page name="home">
     {/* Top Navbar */}
-    <Navbar large sliding={false}>
+    <Navbar small sliding={false}>
       <NavLeft>
-        <Link iconIos="f7:menu" iconAurora="f7:menu" iconMd="material:menu" panelOpen="left" />
+        <img src="images/Mapedia.svg" />
       </NavLeft>
       <NavTitle sliding>Map'edia</NavTitle>
-      <NavTitleLarge>Map'edia</NavTitleLarge>
+
     </Navbar>
     {/* Toolbar */}
     <Toolbar bottom>
       <Link href="/about/">About</Link>
-      <Link href="/form/">Form</Link>
+      {/*<Link href="/form/">Form</Link> */}
     </Toolbar>
     {/* Page content */}
+
+
     <MapObj />
-    
-    
+
+
   </Page>
 );
 export default HomePage;
