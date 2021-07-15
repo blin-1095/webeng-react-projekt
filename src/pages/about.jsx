97427f7a395b859ed133aa6793555ab01f23757a
Alessandra Woods <alessandrawoods@gmail.com>
2021-07-15 15:02:35 -0400

    update profile pictures

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -14,7 +14,9 @@ const AboutPage = () => (
       <p>This PWA was completed in part for the DHBW Ravensburg Campus Friedrichshafen’s Web Engineering 2 course with Professor Friedhelm Koch in 2021.</p>
     </Block>
     <BlockTitle>How the Wiki Works</BlockTitle>
-    <p>Our Map'edia function works with a connection between our map service and a Wikipedia API. To choose what to display to you, our program displays which information is most important to that location (city information, historical ties, etc). As this project was done in Germany, we also chose to display the German Wikipedia site. Therefore, if a location in question is not in the German Wikipedia, it will not be displayed.</p>
+      <Block strong>      
+        <p>Our Map'edia function works with a connection between our map service and a Wikipedia API. To choose what to display to you, our program displays which information is most important to that location (city information, historical ties, etc). As this project was done in Germany, we also chose to display the German Wikipedia site. Therefore, if a location in question is not in the German Wikipedia, it will not be displayed. If you are unable to retrieve information on a single click, please try clicking an extra time.</p>
+      </Block>
     <BlockTitle>The Map'edia Team</BlockTitle>
     <Block strong>
       <Row>
@@ -36,6 +38,8 @@ const AboutPage = () => (
             <div class="card-footer">Daniel Zelesnov</div>
           </div>
         </Col>
+        </Row>
+        <Row>
         <Col>
           <div class="card">
             <div class="card-header">Navigation</div>
@@ -54,11 +58,13 @@ const AboutPage = () => (
             <div class="card-footer">Benedikt Doerflein</div>
           </div>
         </Col>
+        </Row>
+        <Row>
         <Col>
           <div class="card">
             <div class="card-header">Wiki-Expert</div>
             <div class="card-content">
-              <img src={"images/testimage.png"} width="100%" class="lazy lazy-fadeIn"></img>
+              <img src={"images/testimage.png"} width="50%" class="lazy lazy-fadeIn"></img>
             </div>
             <div class="card-footer">Pascal Wildermuth</div>
           </div>
